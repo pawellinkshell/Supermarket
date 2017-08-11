a595e73926de029f4d8d4690b1dee0415a0d1415
Jan Koszela <jan.koszela@hicron.com>
2017-08-11 02:09:13 +0200

    Unwrap lombok annotation for maven issue

--- src/main/java/pl/koszela/jan/domain/Product.java
+++ src/main/java/pl/koszela/jan/domain/Product.java
@@ -1,17 +1,10 @@
 package pl.koszela.jan.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * Created on 11.08.2017.
  *
  * @author Jan Koszela
  */
-@Builder
-@Getter
-@Setter
 public class Product {
 
   private int id;
@@ -19,6 +12,18 @@ public class Product {
   private double unitPrice;
   private String currency;
   private boolean multipricing;
+
+  Product(int id, String item, double unitPrice, String currency, boolean multipricing) {
+    this.id = id;
+    this.item = item;
+    this.unitPrice = unitPrice;
+    this.currency = currency;
+    this.multipricing = multipricing;
+  }
+
+  public static ProductBuilder builder() {
+    return new ProductBuilder();
+  }
 
   @Override
   public String toString() {
@@ -29,5 +34,92 @@ public class Product {
         ", currency='" + currency + '\'' +
         ", multipricing=" + multipricing +
         '}';
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getItem() {
+    return this.item;
+  }
+
+  public double getUnitPrice() {
+    return this.unitPrice;
+  }
+
+  public String getCurrency() {
+    return this.currency;
+  }
+
+  public boolean isMultipricing() {
+    return this.multipricing;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setItem(String item) {
+    this.item = item;
+  }
+
+  public void setUnitPrice(double unitPrice) {
+    this.unitPrice = unitPrice;
+  }
+
+  public void setCurrency(String currency) {
+    this.currency = currency;
+  }
+
+  public void setMultipricing(boolean multipricing) {
+    this.multipricing = multipricing;
+  }
+
+  public static class ProductBuilder {
+
+    private int id;
+    private String item;
+    private double unitPrice;
+    private String currency;
+    private boolean multipricing;
+
+    ProductBuilder() {
+    }
+
+    public Product.ProductBuilder id(int id) {
+      this.id = id;
+      return this;
+    }
+
+    public Product.ProductBuilder item(String item) {
+      this.item = item;
+      return this;
+    }
+
+    public Product.ProductBuilder unitPrice(double unitPrice) {
+      this.unitPrice = unitPrice;
+      return this;
+    }
+
+    public Product.ProductBuilder currency(String currency) {
+      this.currency = currency;
+      return this;
+    }
+
+    public Product.ProductBuilder multipricing(boolean multipricing) {
+      this.multipricing = multipricing;
+      return this;
+    }
+
+    public Product build() {
+      return new Product(id, item, unitPrice, currency, multipricing);
+    }
+
+    public String toString() {
+      return "pl.koszela.jan.domain.Product.ProductBuilder(id=" + this.id + ", item=" + this.item
+          + ", unitPrice=" + this.unitPrice + ", currency=" + this.currency + ", multipricing="
+          + this.multipricing + ")";
+    }
   }
 }
--- src/main/java/pl/koszela/jan/domain/SpecialPrice.java
+++ src/main/java/pl/koszela/jan/domain/SpecialPrice.java
@@ -1,20 +1,81 @@
 package pl.koszela.jan.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * Created on 11.08.2017.
  *
  * @author Jan Koszela
  */
-@Builder
-@Getter
-@Setter
 public class SpecialPrice {
 
   private int id;
   private int amount;
   private double price;
+
+  SpecialPrice(int id, int amount, double price) {
+    this.id = id;
+    this.amount = amount;
+    this.price = price;
+  }
+
+  public static SpecialPriceBuilder builder() {
+    return new SpecialPriceBuilder();
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public int getAmount() {
+    return this.amount;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public static class SpecialPriceBuilder {
+
+    private int id;
+    private int amount;
+    private double price;
+
+    SpecialPriceBuilder() {
+    }
+
+    public SpecialPrice.SpecialPriceBuilder id(int id) {
+      this.id = id;
+      return this;
+    }
+
+    public SpecialPrice.SpecialPriceBuilder amount(int amount) {
+      this.amount = amount;
+      return this;
+    }
+
+    public SpecialPrice.SpecialPriceBuilder price(double price) {
+      this.price = price;
+      return this;
+    }
+
+    public SpecialPrice build() {
+      return new SpecialPrice(id, amount, price);
+    }
+
+    public String toString() {
+      return "pl.koszela.jan.domain.SpecialPrice.SpecialPriceBuilder(id=" + this.id + ", amount="
+          + this.amount + ", price=" + this.price + ")";
+    }
+  }
 }
