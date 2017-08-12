ac4517025955a8ca34075cc97c04fcf60526787e
Jan Koszela <jan.koszela@hicron.com>
2017-08-12 22:49:27 +0200

    Add constants class

---
+++ src/main/java/pl/koszela/jan/constans/SupermarketAttributeConstans.java
@@ -0,0 +1,13 @@
+package pl.koszela.jan.constans;
+
+/**
+ * Created on 12.08.2017.
+ *
+ * @author Jan Koszela
+ */
+public class SupermarketAttributeConstans {
+
+  public static final String SERVLET_NAME_KEY = "servletPath";
+  public static final String SERVLET_NAME_VALUE = "Supermarket";
+
+}
