716c2390be4e5cad6a16b5e35bdd6762e91ed705
Jan Koszela <jan.koszela@hicron.com>
2017-08-13 22:10:44 +0200

    Update cart for item when value is 0

--- src/main/java/pl/koszela/jan/service/impl/DefaultOrderService.java
+++ src/main/java/pl/koszela/jan/service/impl/DefaultOrderService.java
@@ -55,14 +55,18 @@ public class DefaultOrderService implements OrderService {
       Price totalPrice = calculateTotalPrice(order.getProduct(), order.getQuantity());
       order.setTotalPrice(totalPrice);
 
-      if(totalPrice instanceof SpecialPrice){
+      if (totalPrice instanceof SpecialPrice) {
         order.setSpecialOffer(true);
       }
 
       return this.orders.add(order);
 
     } else {
-      orders.remove(getIdFromOrders(order));
+
+      int idFromOrders = getIdFromOrders(order);
+      if (idFromOrders > Integer.MIN_VALUE) {
+        orders.remove(idFromOrders);
+      }
     }
 
     return false;
@@ -118,12 +122,21 @@ public class DefaultOrderService implements OrderService {
   public void updateOrder(Order newOrder) {
     Order foundOrder = findOrderByName(newOrder.getProduct().getName());
     if (foundOrder != null) {
-      if (newOrder.getQuantity() > 0) {
-        orders.set(getIdFromOrders(foundOrder), newOrder);
-      } else {
-        orders.remove(getIdFromOrders(foundOrder));
+      int idFromOrders = getIdFromOrders(foundOrder);
+
+      if (isOutOfArray(idFromOrders)) {
+        if (newOrder.getQuantity() > 0) {
+          orders.set(idFromOrders, newOrder);
+        } else {
+          orders.remove(idFromOrders);
+        }
       }
+
     }
+  }
+
+  private boolean isOutOfArray(int idFromOrders) {
+    return idFromOrders > Integer.MIN_VALUE;
   }
 
   private int getIdFromOrders(Order order) {
@@ -133,7 +146,7 @@ public class DefaultOrderService implements OrderService {
       }
     }
 
-    throw new IndexOutOfBoundsException("Method should be null-checked before in calls");
+    return Integer.MIN_VALUE;
   }
 
   @Override
