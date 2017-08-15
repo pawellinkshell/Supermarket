cd8da92106d244dc46c3800ec573f9cf2ea14ec4
Jan Koszela <jan.koszela@hicron.com>
2017-08-15 20:36:36 +0200

    Add CartFacade

---
+++ src/main/java/pl/koszela/jan/layer/facade/CartFacade.java
@@ -0,0 +1,10 @@
+package pl.koszela.jan.layer.facade;
+
+/**
+ * Created on 15.08.2017.
+ *
+ * @author Jan Koszela
+ */
+public interface CartFacade {
+
+}
---
+++ src/main/java/pl/koszela/jan/layer/facade/impl/DefaultCartFacade.java
@@ -0,0 +1,12 @@
+package pl.koszela.jan.layer.facade.impl;
+
+import pl.koszela.jan.layer.facade.CartFacade;
+
+/**
+ * Created on 15.08.2017.
+ *
+ * @author Jan Koszela
+ */
+public class DefaultCartFacade implements CartFacade {
+
+}
