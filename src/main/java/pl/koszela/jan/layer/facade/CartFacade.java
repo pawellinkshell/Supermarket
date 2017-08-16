6f943140b7d3fa855773d28b23ec0ca945aae31c
Jan Koszela <jan.koszela@hicron.com>
2017-08-16 02:54:30 +0200

    Create cart facade

--- src/main/java/pl/koszela/jan/layer/facade/CartFacade.java
+++ src/main/java/pl/koszela/jan/layer/facade/CartFacade.java
@@ -1,10 +1,18 @@
 package pl.koszela.jan.layer.facade;
 
+import java.util.List;
+import pl.koszela.jan.layer.facade.dto.CartDTO;
+import pl.koszela.jan.layer.facade.dto.OrderDTO;
+
 /**
  * Created on 15.08.2017.
  *
  * @author Jan Koszela
  */
 public interface CartFacade {
+
+  List<OrderDTO> getOrdersFromCart();
+
+  CartDTO getCart();
 
 }
--- src/main/java/pl/koszela/jan/layer/facade/impl/DefaultCartFacade.java
+++ src/main/java/pl/koszela/jan/layer/facade/impl/DefaultCartFacade.java
@@ -1,6 +1,13 @@
 package pl.koszela.jan.layer.facade.impl;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.koszela.jan.layer.facade.CartFacade;
+import pl.koszela.jan.layer.facade.dto.CartDTO;
+import pl.koszela.jan.layer.facade.dto.OrderDTO;
+import pl.koszela.jan.layer.facade.mapper.Mapper;
+import pl.koszela.jan.layer.facade.mapper.impl.Maps;
+import pl.koszela.jan.layer.service.CartService;
 import pl.koszela.jan.main.annotation.Facade;
 
 /**
@@ -8,7 +15,29 @@ import pl.koszela.jan.main.annotation.Facade;
  *
  * @author Jan Koszela
  */
-@Facade
+@Facade("cartFacade")
 public class DefaultCartFacade implements CartFacade {
 
+  private CartDTO cart;
+
+  @Autowired
+  private CartService cartService;
+
+  @Override
+  public List<OrderDTO> getOrdersFromCart() {
+    return null;
+  }
+
+  @Override
+  public CartDTO getCart() {
+
+    if ((cart == null) ||
+        (cart.hashCode() != cartService.getCart().hashCode())) {
+
+      Mapper cartMapper = Maps.getMapper(CartDTO.class);
+      cart = (CartDTO) cartMapper.map(cartService.getCart());
+    }
+
+    return cart;
+  }
 }
